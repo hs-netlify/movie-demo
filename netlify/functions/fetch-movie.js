afee6c5245f407c3636c37c08ac8af5e75b5f412
Henry Smith <henry.smith@netlify.com>
2022-04-01 13:52:43 +0100

    test endpoint console

--- netlify/functions/fetch-movie.js
+++ netlify/functions/fetch-movie.js
@@ -3,6 +3,7 @@ const fetch = (...args) =>
 exports.handler = async (event) => {
   const { movieId } = event.queryStringParameters;
   const endpoint = `${process.env.API_URL}movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`;
+  console.log("endpoint :", endpoint);
   let res = await (await fetch(endpoint)).json();
   return { statusCode: 200, body: JSON.stringify(res) };
 };
