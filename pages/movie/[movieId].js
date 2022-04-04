34c38d0042fb5f37b3a62ca096f152804004ab2b
Henry Smith <henry.smith@netlify.com>
2022-04-04 15:47:11 +0100

    Test static paths

--- pages/movie/[movieId].js
+++ pages/movie/[movieId].js
@@ -37,6 +37,7 @@ export const getStaticPaths = async () => {
 };
 
 const Movie = ({ movie }) => {
+  if (!movie) return <></>;
   return (
     <>
       <BreadCrumb movieTitle={movie.original_title} />
