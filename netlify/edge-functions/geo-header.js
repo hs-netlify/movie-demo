82689cb70d71e470f3baab147af36c316c252c15
Henry Smith <henry.smith@netlify.com>
2022-05-27 08:53:13 +0100

    Context log for edge functions

--- netlify/edge-functions/geo-header.js
+++ netlify/edge-functions/geo-header.js
@@ -1,5 +1,5 @@
 export default async (request, context) => {
-  context.log("This is getting called now");
+  context.log("Geo header function called: ", JSON.stringify(geo));
   return context.json({
     geo: context.geo,
     header: request.headers.get("x-nf-geo"),
