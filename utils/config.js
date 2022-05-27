c834a43d649d869e884f6429632c28b110523646
Henry Smith <henry.smith@netlify.com>
2022-05-27 08:59:01 +0100

    Remove env dependancy

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = "https://api.themoviedb.org/3/";
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const API_KEY = "c3fa7fdc1adfb5196c321ed84e16c1a5";
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
