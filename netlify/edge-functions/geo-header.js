345c57b1ac3d29d29ebc58d844ed56594c41ecdb
Henry Smith <henry.smith@netlify.com>
2022-05-27 09:05:36 +0100

    Geo edge handler

--- netlify/edge-functions/geo-header.js
+++ netlify/edge-functions/geo-header.js
@@ -1,5 +1,5 @@
 export default async (request, context) => {
-  context.log("Geo header function called: ", JSON.stringify(geo));
+  context.log("Geo header function called: ", JSON.stringify(context.geo));
   return context.json({
     geo: context.geo,
     header: request.headers.get("x-nf-geo"),
