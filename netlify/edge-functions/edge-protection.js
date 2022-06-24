abec50d277a310a325e78d4d06f7ad9b44fa938d
Henry Smith <henry.smith@netlify.com>
2022-06-24 11:56:34 +0100

    Add comments to snipped

--- netlify/edge-functions/edge-protection.js
+++ netlify/edge-functions/edge-protection.js
@@ -1,24 +1,25 @@
 export default async (request, context) => {
+  //Import allow lists from Env variables
   const geoAllowList = JSON.parse(Deno.env.get("GEO_ALLOW_LIST"));
   const ipAllowList = JSON.parse(Deno.env.get("IP_ALLOW_LIST"));
 
+  //Fetch geo code and IP from incoming request
   const geoCode = context.geo?.country?.code;
   const ip = context.ip;
 
-  console.log(typeof geoAllowList);
-
-  // Uses https://www.ipqualityscore.com/documentation/proxy-detection/overview
-
+  // Uses https://www.ipqualityscore.com/documentation/proxy-detection/overview to check for VPN and BOT traffic.
   const ipCheck = await (
     await fetch(
       `https://ipqualityscore.com/api/json/ip/abvO9dUu9Ej84y2TOG0UpWwHJtYfwnFI/${ip}?strictness=1&allow_public_access_points=true`
     )
   ).json();
 
+  //Complete checks against lists and fraud score
   const correctGeo = geoAllowList.includes(geoCode);
   const correctIp = ipAllowList.includes(ip);
   const lowFraudScore = ipCheck.fraud_score < 50;
 
+  //Allows/Denies Access and rewrites to respective pages.
   if (correctGeo && lowFraudScore && correctIp) {
     context.log(
       `Access Granted - IP Address ${ip} is allowed from ${geoCode}.Fraud score = ${ipCheck.fraud_score}`
