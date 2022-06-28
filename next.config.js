f0b2696d9484f09f1782aa6f07c4aa4a7ef05741
Henry Smith <henry.smith@netlify.com>
2022-06-28 14:26:40 +0100

    Dont require env

--- next.config.js
+++ next.config.js
@@ -1,14 +1,26 @@
 module.exports = {
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-  },
-  async rewrites() {
-    return [
+  i18n: {
+    locales: ['en-US', 'fr', 'nl-NL', 'nl-BE'],
+    defaultLocale: 'en-US',
+
+    domains: [
       {
-        source: "/working",
-        destination: `/.netlify/builders/sitemap`,
+        // Note: subdomains must be included in the domain value to be matched
+        // e.g. www.example.com should be used if that is the expected hostname
+        domain: 'example.com',
+        defaultLocale: 'en-US',
       },
-    ];
+      {
+        domain: 'example.fr',
+        defaultLocale: 'fr',
+      },
+      {
+        domain: 'example.nl',
+        defaultLocale: 'nl-NL',
+        // specify other locales that should be redirected
+        // to this domain
+        locales: ['nl-BE'],
+      },
+    ],
   },
-};
+}
