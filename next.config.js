e4f1c598b1b7d79be32b570573550c593ec11d4c
Henry Smith <henry.smith@netlify.com>
2022-07-11 14:10:36 +0100

    Update build id

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
 module.exports = {
-  i18n: {
-    locales: ['en-US', 'fr', 'nl-NL', 'nl-BE'],
-    defaultLocale: 'en-US',
-
-    domains: [
-      {
-        // Note: subdomains must be included in the domain value to be matched
-        // e.g. www.example.com should be used if that is the expected hostname
-        domain: 'example.com',
-        defaultLocale: 'en-US',
-      },
-      {
-        domain: 'example.fr',
-        defaultLocale: 'fr',
-      },
-      {
-        domain: 'example.nl',
-        defaultLocale: 'nl-NL',
-        // specify other locales that should be redirected
-        // to this domain
-        locales: ['nl-BE'],
-      },
-    ],
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return "const-id";
   },
-}
+};
