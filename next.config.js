503348f0d99f9e659499776635140bf280922dfc
Henry Smith <henry.smith@netlify.com>
2022-07-21 17:05:06 +0100

    Test header next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const securityHeaders = [
+  {
+    key: "Test-Henry-Next",
+    value: "true",
+  },
+];
+
 module.exports = {
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
@@ -5,5 +12,14 @@ module.exports = {
   },
   images: {
     domains: ["image.tmdb.org"],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
