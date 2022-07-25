b05931d61e7f038281837bc964e27ade12150972
Henry Smith <henry.smith@netlify.com>
2022-07-25 13:33:50 +0100

    Update head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import "/styles/GlobalStyle.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src="https://www.googleoptimize.com/optimize.js?id=OPT-KC2PT6F"
+        ></script>
+        <title>Working</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
