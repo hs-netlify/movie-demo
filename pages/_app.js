8197de9d7c8e649ba0c6d52164f99feb65b91598
Henry Smith <henry.smith@netlify.com>
2022-07-25 13:44:45 +0100

    add document

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,7 @@
 import "/styles/GlobalStyle.css";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <script
-          async
-          src="https://www.googleoptimize.com/optimize.js?id=OPT-KC2PT6F"
-        ></script>
-        <title>Working</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-LHHZG5392C`}
+          />
+          <script
+            async
+            src="https://www.googleoptimize.com/optimize.js?id=OPT-KC2PT6F"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LHHZG5392C', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
