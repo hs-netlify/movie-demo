953ac205be1bcc1da2ab2fcbd142c8c8f6622a5c
Henry Smith <henry.smith@netlify.com>
2022-07-26 13:29:52 +0100

    Push test b

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ const Home = ({ movies }) => {
   const randFilm = day <= 19 ? day : 19;
   return (
     <>
-      <div className="text-center text-4xl p-4">TEST A</div>
+      <div className="text-center text-4xl p-4">TEST B</div>
       <Header></Header>
       {!searchTerm && state.results[randFilm] ? (
         <HeroImage
