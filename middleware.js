780d406124420f294d55f77d10f385211187e6c8
Henry Smith <henry.smith@netlify.com>
2022-07-28 10:21:49 +0100

    Fix middleware

--- middleware.js
+++ middleware.js
@@ -3,29 +3,29 @@ import { NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
   const path = request.nextUrl.pathname;
-  const response = NextResponse.next();
+
   console.log(path);
 
   const bucketName = "test_bucket";
-  const bucket = request.cookies[bucketName];
+  const bucket = request.cookies.get(bucketName);
+  console.log("bucket", bucket);
 
   if (bucket) {
     return NextResponse.rewrite(
       new URL(path, `https://${bucket}--next-movie-db.netlify.app/`)
     );
+  } else {
+    const weighting = 0.5;
+
+    const random = Math.random();
+    const newBucketValue = random <= weighting ? "a" : "b";
+
+    let response = NextResponse.rewrite(
+      new URL(path, `https://${newBucketValue}--next-movie-db.netlify.app/`)
+    );
+    response.cookies.set(bucketName, newBucketValue);
+    console.log("Response", response);
+
+    return response;
   }
-
-  const weighting = 0.5;
-
-  const random = Math.random();
-  const newBucketValue = random <= weighting ? "a" : "b";
-
-  response.cookies.set({
-    name: bucketName,
-    value: newBucketValue,
-  });
-
-  return NextResponse.rewrite(
-    new URL(path, `https://${newBucketValue}--next-movie-db.netlify.app/`)
-  );
 }
