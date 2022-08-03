ec9e9e9c0a5ac3ea5e4af1765a6d1d5a842b4fc9
Henry Smith <henry.smith@netlify.com>
2022-08-03 12:30:32 +0100

    Redirect tested

--- netlify/edge-functions/subdomain-redirect.js
+++ netlify/edge-functions/subdomain-redirect.js
@@ -1,10 +1,10 @@
-export default (request) => {
+export default (request, context) => {
   const subdomain = request.url.match(/https:\/\/(.*?)\-\-/);
   const redirected = request.headers.get("x-cloudflare-redirect");
   console.log("redirect", redirected);
   if (subdomain && !redirected) {
     return Response.redirect(`https://${subdomain[1]}.netlify-se-demo.com/`);
   } else {
-    return;
+    return context.next();
   }
 };
