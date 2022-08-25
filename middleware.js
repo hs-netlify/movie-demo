af847eb6b1d4430a0b66cb64ab5ce36ebafb23b9
Henry Smith <henry.smith@netlify.com>
2022-08-25 17:54:13 +0100

    Add middleware!

--- middleware.js
+++ middleware.js
@@ -2,16 +2,16 @@ import { NextResponse } from "next/server";
 import { MiddlewareRequest } from "@netlify/next";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(NextRequest) {
+export async function middleware(NextRequest) {
   const request = new MiddlewareRequest(NextRequest);
-  const response = await request.next()
+  const response = await request.next();
   response.setPageProp("title", "WOW A NEW TITLE!");
-  console.log('Gets here');
+  console.log("Gets here");
   console.log(response);
   return response;
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/*",
+  matcher: "/",
 };
