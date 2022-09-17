c7b2c9e90fd24ca2e199c9d85c0ac7ffad2cb7bf
Henry Smith <henry.smith@netlify.com>
2022-09-16 15:43:26 -1000

    Update

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ const Home = ({ movies }) => {
 
   if (error) return <div>Something Went Wrong</div>;
   const day = new Date().getDate();
-  const randFilm = day <= 18 ? day : 19;
+  const randFilm = day <= 19 ? day : 19;
   return (
     <>
       <Header></Header>
