ed07e97f9c1a5775a373b94e019ff941ee7c228f
Henry Smith <henry.smith@netlify.com>
2022-09-20 10:20:56 +0100

    Console logging for EFs

--- plugins/load-ef/index.js
+++ plugins/load-ef/index.js
@@ -1,11 +1,15 @@
+const { isCompositeComponent } = require("react-dom/test-utils");
+
 module.exports = {
   onPreBuild: ({ netlifyConfig }) => {
     if (!netlifyConfig.build.environment.SKIP_EF == "true") {
+      console.log("Deploying Edge functions for redirects");
       if (netlifyConfig.edge_functions) {
         netlifyConfig["edge_functions"].push({
           path: "/*",
           function: "example-ef",
         });
+        console.log("Edge function deployed");
       } else {
         netlifyConfig["edge_functions"] = [
           {
@@ -13,7 +17,10 @@ module.exports = {
             function: "example-ef",
           },
         ];
+        console.log("Edge function deployed");
       }
+    } else {
+      conosle.log("Skipping Edge functions deployment");
     }
   },
 };
