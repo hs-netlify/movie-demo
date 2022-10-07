14fdd775e60327226a6e731f50765d3713c33550
Henry Smith <henry.smith@netlify.com>
2022-10-07 10:12:33 +0100

    Update code snippets

--- netlify/edge-functions/ab-test.js
+++ netlify/edge-functions/ab-test.js
@@ -1,18 +1,20 @@
 export default async (request, context) => {
   let buckets = JSON.parse(Deno.env.get("AB_TEST_LIST") || "null");
 
+  //If environment variable not set return standard page
   if (!buckets) {
     return context.next();
   }
 
+  //Ensure weighting adds up to 1
   let totalWeighting = buckets.reduce(
     (tot, bucket) => tot + bucket.weighting,
     0
   );
-
   let weightingMultiplier = totalWeighting == 1 ? 1 : 1 / totalWeighting;
 
-  const bucketName = "ab-bucket";
+  //Set the cookie name of the bucket
+  const bucketName = "netlify-split-test";
 
   const requestUrl = new URL(request.url);
 
@@ -20,9 +22,9 @@ export default async (request, context) => {
   let bucket = context.cookies.get(bucketName);
   let hasBucket = !!bucket;
 
-  let randomNumber = Math.random();
-
+  //Assign a bucket if the cookie has not been set
   if (!bucket) {
+    let randomNumber = Math.random();
     let totalWeighting = 0;
     buckets.forEach((b) => {
       if (
@@ -37,13 +39,14 @@ export default async (request, context) => {
     });
   }
 
+  //Generate full proxy url
   let url = `${bucket}${requestUrl.pathname}`;
 
+  //Set cookie if new bucket has been set
   if (!hasBucket) {
     context.cookies.set({ name: bucketName, value: bucket });
   }
 
   let proxyResponse = await fetch(url);
-
   return new Response(proxyResponse.body, proxyResponse);
 };
