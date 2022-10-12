900fa84b60d68f276b9256839996e854e8ed3e4a
Henry Smith <henry.smith@netlify.com>
2022-10-12 16:22:50 +0100

    Latest code

--- pages/index.js
+++ pages/index.js
@@ -61,6 +61,7 @@ const Home = ({ movies }) => {
   const randFilm = day <= 19 ? day : 19;
   return (
     <>
+      <div className="text-center text-3xl bg-red-400">NEW TITLE</div>
       <Header></Header>
       {!searchTerm && state.results[randFilm] ? (
         <HeroImage
