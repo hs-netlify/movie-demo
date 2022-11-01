ad65c1d51641d060b0365059ac2a913e73c1df91
Henry Smith <henry.smith@netlify.com>
2022-11-01 09:58:18 +0100

    Change header

--- pages/index.js
+++ pages/index.js
@@ -61,6 +61,7 @@ const Home = ({ movies }) => {
   const randFilm = day <= 19 ? day : 19;
   return (
     <>
+      <div className="text-center text-4xl bg-red-400">DEVELPMENT</div>
       <Header></Header>
       {!searchTerm && state.results[randFilm] ? (
         <HeroImage
