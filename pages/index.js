2bcb9c51a62c5c762d03d7dd582b5a56eb18570a
Henry Smith <henry.smith@netlify.com>
2023-01-11 14:17:58 +0000

    Remove ehader

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,6 @@ const Home = ({ movies }) => {
   const randFilm = day <= 19 ? day : 19;
   return (
     <>
-      <div className="text-center bg-red-400 text-white">DEV BRANCH</div>
       <Header></Header>
       {!searchTerm && state.results[randFilm] ? (
         <HeroImage
