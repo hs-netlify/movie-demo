67a8b8ba000f192c83cf0b6e23b8342e158944c5
Henry Smith <henry.smith@netlify.com>
2023-02-03 13:03:55 +0000

    Remove header

--- pages/index.js
+++ pages/index.js
@@ -58,9 +58,6 @@ const Home = ({ movies }) => {
   const randFilm = day <= 19 ? day : 19;
   return (
     <>
-      <div className="text-center bg-red-400 text-3xl text-white">
-        Dev Branch
-      </div>
       <Header></Header>
       {!searchTerm && state.results[randFilm] ? (
         <HeroImage
