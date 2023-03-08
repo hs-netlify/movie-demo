429d39d06cd2947209b10bf5cfb5fdc18cbb119b
Henry Smith <henry.smith@netlify.com>
2023-03-08 08:46:25 +0000

    Add test functions

---
+++ netlify/functions/test/test.js
@@ -0,0 +1,19 @@
+const { builder } = require("@netlify/functions");
+
+async function handler(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html",
+    },
+    body: `
+    <!DOCTYPE html>
+      <html>
+        <body>
+          Hello World
+        </body>
+    </html>
+    `,
+  };
+}
+exports.handler = builder(handler);
