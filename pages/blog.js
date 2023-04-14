dd1608c7a898ae8d8e81f35bbe5b86d5b55a6793
Henry Smith <henry.smith@netlify.com>
2023-04-14 16:47:10 +0100

    No delay test

--- pages/blog.js
+++ pages/blog.js
@@ -1,18 +1,6 @@
 const Blog = () => <div>My blog that works</div>;
 
 export async function getStaticProps() {
-  // Add your data fetching logic here
-
-  async function dummyAwaitFunction() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Dummy data fetched after a delay");
-      }, 2000); // 2 seconds delay
-    });
-  }
-
-  await dummyAwaitFunction();
-
   return {
     props: {
       // Pass your fetched data as props to your component
