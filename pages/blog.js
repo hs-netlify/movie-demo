cb157973860bd2270f16720d58b612e0b555c105
Henry Smith <henry.smith@netlify.com>
2023-04-14 16:37:43 +0100

    Test with delay

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,16 @@
 const Blog = () => <div>My blog that works</div>;
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://www.gov.uk/bank-holidays.json");
-  const data = await res.json();
+  const delayedFetch = new Promise(async (resolve) => {
+    const res = await fetch("https://www.gov.uk/bank-holidays.json");
+    const data = await res.json();
+
+    setTimeout(() => {
+      resolve(data);
+    }, 2000);
+  });
+
+  let data = await delayedFetch();
   return { props: data };
 };
 
