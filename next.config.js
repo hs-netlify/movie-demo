27510d3a539cc553b0377dfffd49fd01a041f6d4
Henry Smith <henry.smith@netlify.com>
2023-06-07 15:58:48 +0100

    Update to use base url config"

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const base =   process.env.NETLIFY  ? process.env.CONTEXT === "production"
+      ? "https://db.netlify-se-test.com"
+      : process.env.DEPLOY_PRIME_URL
+    : undefined,
+
 module.exports = {
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
@@ -9,6 +14,14 @@ module.exports = {
       ? "https://db.netlify-se-test.com"
       : process.env.DEPLOY_PRIME_URL
     : undefined,
+  async rewrites() {
+    return [
+      {
+        source: '/_next/data/*:path',
+        to: `${base}/_next/data/*:path`
+      }
+    ];
+  },
   images: {
     domains: ["image.tmdb.org"],
   },
