16f71f68f42aed5654e2ee19641d0e06182a86e7
Henry Smith <henry.smith@netlify.com>
2023-06-07 13:54:29 +0100

    Update next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ module.exports = {
     // You can, for example, get the latest git commit hash here
     return "const-id";
   },
+
+  assetPrefix: "shop",
   images: {
     domains: ["image.tmdb.org"],
   },
