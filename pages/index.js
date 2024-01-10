9b385f96ef90b3d3fa2a880d8831a8143b12cdff
Henry Smith <henry.smith@netlify.com>
2024-01-10 12:00:55 +0000

    reset

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,6 @@ const Home = ({ movies }) => {
   const randFilm = day <= 19 ? day : 12;
   return (
     <>
-      <div>DEV BRANCH</div>
       <Header></Header>
       {!searchTerm && state.results[randFilm] ? (
         <HeroImage
